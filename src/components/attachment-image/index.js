9487ccbd609e84d79e4c636ff8cca0371547b8f6
Dan Northern <git@dannorthern.com>
2024-09-10 00:26:34 -0400

    Fix so props can override default parameters.

--- src/components/attachment-image/index.js
+++ src/components/attachment-image/index.js
@@ -20,33 +20,34 @@ import { useSelect } from "@wordpress/data";
  * @param {string} props.size The size of the image to display. Defaults to 'full'.
  * @returns {*} React JSX
  */
-function AttachmentImage({ imageId, size = "full" }) {
-	const { image } = useSelect((select) => ({
-		image: select("core").getMedia(imageId),
-	}));
+function AttachmentImage({ imageId, size = "full", ...overrideProps }) {
+    const { image } = useSelect((select) => ({
+        image: select("core").getMedia(imageId),
+    }));
 
-	const imageAttributes = () => {
-		let attributes = {
-			src: image.source_url,
-			alt: image.alt_text,
-			className: `attachment-${size} size-${size}`,
-			width: image.media_details.width,
-			height: image.media_details.height,
-		};
-		if (
-			image.media_details &&
-			image.media_details.sizes &&
-			image.media_details.sizes[size]
-		) {
-			attributes.src = image.media_details.sizes[size].source_url;
-			attributes.width = image.media_details.sizes[size].width;
-			attributes.height = image.media_details.sizes[size].height;
-		}
+    const imageAttributes = () => {
+        let attributes = {
+            src: image.source_url,
+            alt: image.alt_text,
+            className: `attachment-${size} size-${size}`,
+            width: image.media_details.width,
+            height: image.media_details.height,
+        };
+        if (
+            image.media_details &&
+            image.media_details.sizes &&
+            image.media_details.sizes[size]
+        ) {
+            attributes.src = image.media_details.sizes[size].source_url;
+            attributes.width = image.media_details.sizes[size].width;
+            attributes.height = image.media_details.sizes[size].height;
+        }
 
-		return attributes;
-	};
+        // Override with any manually set props
+        return { ...attributes, ...overrideProps };
+    };
 
-	return <>{image && <img {...imageAttributes()} />}</>;
+    return <>{image && <img {...imageAttributes()} />}</>;
 }
 
 // Export Component

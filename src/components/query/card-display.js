d91f76cff59f6f01b83991b2f25e12af17e53bcc
Dan Northern <git@dannorthern.com>
2024-10-25 02:52:20 -0400

    Allow toggles to be optional.

--- src/components/query/card-display.js
+++ src/components/query/card-display.js
@@ -1,24 +1,9 @@
 import { PanelBody, ToggleControl } from "@wordpress/components";
 import { __ } from "@wordpress/i18n";
 
-/**
- * Render the settings block used to select the post types.
- *
- * @since 0.1.0
- *
- * @returns {WPElement} Element to render.
- */
-function QueryCardDisplay(props) {
-	const {
-		attributes: {
-			showImage,
-			showTitle,
-			showExcerpt,
-			showTaxonomy,
-			showButton,
-		},
-		setAttributes,
-	} = props;
+function QueryCardDisplay({ attributes, setAttributes, cardDisplayOptions }) {
+	const { showImage, showTitle, showExcerpt, showTaxonomy, showButton } =
+		attributes;
 
 	return (
 		<PanelBody title={__("Card Content", "built_starter")}>
@@ -28,41 +13,41 @@ function QueryCardDisplay(props) {
 					"built_starter",
 				)}
 			</p>
-			<ToggleControl
-				label="Image"
-				checked={showImage}
-				onChange={(showImageNew) =>
-					setAttributes({ showImage: showImageNew })
-				}
-			/>
-			<ToggleControl
-				label="Title"
-				checked={showTitle}
-				onChange={(showTitleNew) =>
-					setAttributes({ showTitle: showTitleNew })
-				}
-			/>
-			<ToggleControl
-				label="Excerpt"
-				checked={showExcerpt}
-				onChange={(showExcerptNew) =>
-					setAttributes({ showExcerpt: showExcerptNew })
-				}
-			/>
-			<ToggleControl
-				label="Taxonomy"
-				checked={showTaxonomy}
-				onChange={(showTaxonomyNew) =>
-					setAttributes({ showTaxonomy: showTaxonomyNew })
-				}
-			/>
-			<ToggleControl
-				label="Button"
-				checked={showButton}
-				onChange={(showButtonNew) =>
-					setAttributes({ showButton: showButtonNew })
-				}
-			/>
+			{cardDisplayOptions.image && (
+				<ToggleControl
+					label="Image"
+					checked={showImage}
+					onChange={(value) => setAttributes({ showImage: value })}
+				/>
+			)}
+			{cardDisplayOptions.title && (
+				<ToggleControl
+					label="Title"
+					checked={showTitle}
+					onChange={(value) => setAttributes({ showTitle: value })}
+				/>
+			)}
+			{cardDisplayOptions.excerpt && (
+				<ToggleControl
+					label="Excerpt"
+					checked={showExcerpt}
+					onChange={(value) => setAttributes({ showExcerpt: value })}
+				/>
+			)}
+			{cardDisplayOptions.taxonomy && (
+				<ToggleControl
+					label="Taxonomy"
+					checked={showTaxonomy}
+					onChange={(value) => setAttributes({ showTaxonomy: value })}
+				/>
+			)}
+			{cardDisplayOptions.button && (
+				<ToggleControl
+					label="Button"
+					checked={showButton}
+					onChange={(value) => setAttributes({ showButton: value })}
+				/>
+			)}
 		</PanelBody>
 	);
 }
